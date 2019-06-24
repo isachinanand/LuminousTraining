f4cbeb8a1ebaec8bbe9441ce8a7eeb1ee28dd7d2
saanand <saanand@craterzone.com>
2019-06-24 10:36:53 +0530

    Initial Commit

---
+++ LuminousTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LuminousTraining
+//
+//  Created by Sachin Anand on 24/06/19.
+//  Copyright © 2019 Himanshu Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
